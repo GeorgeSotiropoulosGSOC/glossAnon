054b94bec3a7bd88ed1d1cb1fc702c9441838572
GeorgeSotiropoulos <swtiropoulos.geo@gmail.com>
2026-08-24 04:12:21 +0300

    Delete glossAnon directory

--- glossAnon/src/glossanon/api/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Optional FastAPI REST interface for glossanon.
-
-Importing this subpackage requires the ``api`` extra::
-
-    pip install "glossanon[api]"
-
-The app is created with :func:`glossanon.api.app.create_app` and exposed as the
-module-level ``app`` for ``uvicorn glossanon.api.app:app``.
-"""
